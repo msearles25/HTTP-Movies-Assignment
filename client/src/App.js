cbc4e14b22714c942ae5a4448dc482190fa2ea0a
msearles25 <skaman4459@gmail.com>
2019-12-18 18:39:48 -0500

    rendering?

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 
+import axios from 'axios'
+import UpdateMovieForm from "./Movies/UpdateMovieForm";
+
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/movies")
+      .then(res => setMovies(res.data))
+      .catch(err => console.log(err.response));
+  }, [])
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
@@ -14,16 +25,25 @@ const App = () => {
   return (
     <>
       <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
+      {/* <Route exact path="/" component={MovieList} /> */}
+      <Route 
+        exact path="/"
+        render={props => {
+          return <MovieList {...props} movies={movies} setMovies={setMovies}/>
+        }}
+      />
       <Route
         path="/movies/:id"
         render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
+          return <Movie {...props} addToSavedList={addToSavedList} movies={movies}/>;
         }}
       />
       <Route 
         path='/update-movie/:id'
-        
+        render={props => {
+           return <UpdateMovieForm {...props} movies={movies} setMovies={setMovies}/>
+        }}
+       
       />
     </>
   );
